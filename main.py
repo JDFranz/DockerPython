29beacad31ee4d3bd965c2a539b7205bac5be410
Jan Dominik Franz <franzjandominik@gmail.com>
2023-12-31 14:32:10 +0100

    added main as proof of concept

---
+++ main.py
@@ -0,0 +1,37 @@
+import uvicorn
+import os
+from fastapi import Depends, FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def testSSL():
+    # Create a NumPy array with some dummy data
+    dummy_array = np.array([[1, 2, 3], [4, 5, 6]])
+
+    # Convert the array to a string
+    result_string = str(dummy_array)
+
+    return result_string + "this was from numpy"
+
+
+if __name__ == "__main__":
+    if os.path.isfile("./certificates/SSL/KEY.key") and os.path.isfile(
+        "./certificates/SSL/CERT.cer"
+    ):
+        print("https://localhost:443")
+        print("https://localhost:8099")
+        uvicorn.run(
+            "app:app",
+            host="0.0.0.0",
+            port=8099,
+            reload=True,
+            ssl_keyfile="./certificates/SSL/KEY.key",
+            ssl_certfile="./certificates/SSL/CERT.cer",
+        )
+        # uvicorn.run('main:app', host='0.0.0.0' ,port=8099 ,reload = True )
+    else:
+        print("http://localhost:8099")
+        uvicorn.run("app:app", host="0.0.0.0", port=8099, reload=True)
